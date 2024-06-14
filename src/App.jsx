3382e6dca0ca48ff9192fb83876c5f369343961d
Diontae Akins <diontaea@gmail.com>
2024-06-14 13:15:56 -0400

    1

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import NavBar from "./components/NavBar";
+import ProjectDetails from "./pages/ProjectDetails";
+
+//
+import { useState } from "react";
+import { ThemeContext } from "./context/ThemeContext";
+import "./App.css";
+import MainLayout from "./components/MainLayout";
+import UserProvider from "./context/UserContext";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <MainLayout>
+          <UserProvider>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+          </Routes>
+          </UserProvider>
+        </MainLayout>
+      </ThemeContext.Provider>
+    </>
+  );
+}
+
+export default App;
